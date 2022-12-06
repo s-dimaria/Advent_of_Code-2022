1537b008f2c9912ddc2c494b40a1e76f3552a406
s-dimaria <salvatoredimaria99@gmail.com>
2022-12-06 12:04:39 +0100

    Day 6 done Java, Day 1 done Js

---
+++ Java/Day6/src/PuzzlePartOne.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class PuzzlePartOne {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new FileReader("input.txt"));
+
+        int count = 0;
+        int lengthSubSequence = 4;
+        while(in.ready()) {
+
+            char[] buff = new char[lengthSubSequence];
+            int[] intBuff = new int[lengthSubSequence];
+
+            int first = in.read();
+            buff[0] = (char) first;
+            in.mark(0);
+
+            in.read(buff, 1, lengthSubSequence-1);
+
+            Arrays.setAll(intBuff, i -> (int) buff[i]);
+            if(Arrays.stream(intBuff).distinct().count() == lengthSubSequence) {
+                count+=lengthSubSequence;
+                break;
+            }
+            else {
+                in.reset();
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+
+}
--- README.md
+++ README.md
@@ -14,7 +14,7 @@ For all the challenges i will use different type of programming languages :D.
 | 3 | <a href="Java/Day3/src/PuzzlePartOne.java">:heavy_check_mark:</a> | <a href="Java/Day3/src/PuzzlePartTwo.java">:heavy_check_mark:</a> |
 | 4 | <a href="Java/Day4/src/PuzzlePartOne.java">:heavy_check_mark:</a> | <a href="Java/Day4/src/PuzzlePartOne.java">:heavy_check_mark:</a> |
 | 5 | <a href="Java/Day5/src/PuzzlePartOne.java">:heavy_check_mark:</a> | <a href="Java/Day5/src/PuzzlePartOne.java">:heavy_check_mark:</a> |
-| 6 | ❌ | ❌ |
+| 6 | <a href="Java/Day6/src/PuzzlePartOne.java">:heavy_check_mark:</a> | <a href="Java/Day6/src/PuzzlePartOne.java">:heavy_check_mark:</a> |
 | 7 | ❌ | ❌ |
 | 8 | ❌ | ❌ |
 | 9 | ❌ | ❌ |
